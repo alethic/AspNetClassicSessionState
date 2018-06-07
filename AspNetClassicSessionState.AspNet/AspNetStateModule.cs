61233c6835a72b5932195c8545478a4e67251af6
Jerome Haltom <wasabi@cogito.cx>
2018-06-07 10:01:17 -0500

    Clean up handler.

--- AspNetClassicSessionState.AspNet/AspNetStateModule.cs
+++ AspNetClassicSessionState.AspNet/AspNetStateModule.cs
@@ -4,7 +4,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.Remoting;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Web;
-using System.Web.SessionState;
 
 using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 
@@ -31,23 +30,6 @@ namespace AspNetClassicSessionState.AspNet
         }
 
         /// <summary>
-        /// Registered as the handler briefly to force initialization of session state.
-        /// </summary>
-        class EnableSessionStateHandler :
-            IHttpHandler,
-            IRequiresSessionState
-        {
-
-            public bool IsReusable => true;
-
-            public void ProcessRequest(HttpContext context)
-            {
-                // noop
-            }
-
-        }
-
-        /// <summary>
         /// Initializes the instance.
         /// </summary>
         /// <param name="context"></param>
@@ -56,7 +38,6 @@ namespace AspNetClassicSessionState.AspNet
             context.AddOnBeginRequestAsync(BeginOnBeginRequestAsync, EndOnBeginRequestAsync);
             context.AddOnMapRequestHandlerAsync(OnBeginMapRequestHandlerAsync, OnEndMapRequestHandlerAsync);
             context.AddOnAcquireRequestStateAsync(OnBeginAcquireRequestStateAsync, OnEndAcquireRequestStateAsync);
-            context.AddOnPostAcquireRequestStateAsync(OnBeginPostAcquireRequestStateAsync, OnEndPostAcquireRequestStateAsync);
         }
 
         /// <summary>
@@ -115,7 +96,7 @@ namespace AspNetClassicSessionState.AspNet
         {
             // register handler to force session state initialization
             if (HttpContext.Current.Handler == null)
-                HttpContext.Current.Handler = new StaHttpHandlerWithSessionState();
+                HttpContext.Current.Handler = new EnableSessionStateHandler();
 
             return new CompletedAsyncResult(true);
         }
@@ -140,7 +121,7 @@ namespace AspNetClassicSessionState.AspNet
         IAsyncResult OnBeginAcquireRequestStateAsync(object sender, EventArgs e, AsyncCallback cb, object extraData)
         {
             // unmap our temporary state handler.
-            if (HttpContext.Current.Handler is StaHttpHandlerWithSessionState)
+            if (HttpContext.Current.Handler is EnableSessionStateHandler)
                 HttpContext.Current.Handler = null;
 
             return new CompletedAsyncResult(true);
@@ -151,36 +132,6 @@ namespace AspNetClassicSessionState.AspNet
         /// </summary>
         /// <param name="ar"></param>
         void OnEndAcquireRequestStateAsync(IAsyncResult ar)
-        {
-
-        }
-
-        /// <summary>
-        /// Invoked after the session state is acquired.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        /// <param name="cb"></param>
-        /// <param name="extraData"></param>
-        /// <returns></returns>
-        IAsyncResult OnBeginPostAcquireRequestStateAsync(object sender, EventArgs args, AsyncCallback cb, object extraData)
-        {
-            // use an environment variable to communicate Session State Module URI base between ASP.NET and ASP Classic
-            var appDomainIdKey = $"{HttpRuntime.AppDomainAppId}_APPDOMAINID";
-            var appDomainId = Environment.GetEnvironmentVariable(appDomainIdKey, EnvironmentVariableTarget.Process);
-
-            // register environment variable used to discover AppDomain
-            if (string.IsNullOrEmpty(appDomainId))
-                Environment.SetEnvironmentVariable(appDomainIdKey, AppDomain.CurrentDomain.Id.ToString(), EnvironmentVariableTarget.Process);
-
-            return new CompletedAsyncResult(true);
-        }
-
-        /// <summary>
-        /// Invoked after the session state is acquired.
-        /// </summary>
-        /// <param name="ar"></param>
-        void OnEndPostAcquireRequestStateAsync(IAsyncResult ar)
         {
 
         }
