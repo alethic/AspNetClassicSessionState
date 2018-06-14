f564226cddb481d7c211ab94f0077faa15773032
Jerome Haltom <jhaltom@fileandserve.com>
2018-06-13 23:37:57 -0500

    Add FMT and change to multi threaded.

--- AspNetClassicSessionState.Asp/AspNetState.h
+++ AspNetClassicSessionState.Asp/AspNetState.h
@@ -5,7 +5,6 @@
 #include "resource.h"       // main symbols
 #include <asptlb.h>         // Active Server Pages Definitions
 #include <vcclr.h>
-#include <msclr\auto_gcroot.h>
 
 #include "AspNetClassicSessionStateAsp_i.h"
 
@@ -16,14 +15,14 @@ using namespace ATL;
 // CAspNetState
 
 class ATL_NO_VTABLE CAspNetState :
-    public CComObjectRootEx<CComSingleThreadModel>,
+    public CComObjectRootEx<CComMultiThreadModel>,
     public CComCoClass<CAspNetState, &CLSID_AspNetState>,
     public IDispatchImpl<IAspNetState, &IID_IAspNetState, &LIBID_AspNetClassicSessionStateAspLib, /*wMajor =*/ 1, /*wMinor =*/ 0>
 {
 public:
     CAspNetState()
     {
-
+        m_pUnkMarshaler = nullptr;
     }
 
     DECLARE_REGISTRY_RESOURCEID(IDR_ASPNETSTATE)
@@ -32,20 +31,24 @@ public:
     BEGIN_COM_MAP(CAspNetState)
         COM_INTERFACE_ENTRY(IAspNetState)
         COM_INTERFACE_ENTRY(IDispatch)
+        COM_INTERFACE_ENTRY_AGGREGATE(IID_IMarshal, m_pUnkMarshaler.p)
     END_COM_MAP()
 
 
     DECLARE_PROTECT_FINAL_CONSTRUCT()
+    DECLARE_GET_CONTROLLING_UNKNOWN()
 
     HRESULT FinalConstruct()
     {
-        return S_OK;
+        return CoCreateFreeThreadedMarshaler(GetControllingUnknown(), &m_pUnkMarshaler.p);
     }
 
     void FinalRelease()
     {
-
+        m_pUnkMarshaler.Release();
     }
+
+    CComPtr<IUnknown> m_pUnkMarshaler;
 
     // IAspNetState
 public:
